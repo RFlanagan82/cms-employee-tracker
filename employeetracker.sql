daba8c7185d98fb6f74105d6a70c63b134e18a92
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-10-05 16:23:18 -0400

    created employeeseed.sql and employeetracker.sql files with initial values.

---
+++ employeetracker.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employees;
+
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE department (
+id Integer AUTO_INCREMENT,
+name VARCHAR(30),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+id Integer AUTO_INCREMENT,
+title VARCHAR(30),
+salary DECIMAL(5,3),
+department_id INTEGER
+);
+
+CREATE TABLE employee (
+id INTEGER AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INTEGER,
+manager_id INTEGER NULL
+);
